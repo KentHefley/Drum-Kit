9911a1eb45dff2ff4d92d199fae36ca4f980885b
Kent Hefley <hefleykent@gmail.com>
2020-09-10 19:52:26 -0500

    "first commit"

---
+++ app.js
@@ -0,0 +1,27 @@
+//selectors
+const keys = document.querySelectorAll('.key')
+
+
+//event listeners
+window.addEventListener('keydown', playSound);
+
+
+
+//functions
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add('playing');
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
